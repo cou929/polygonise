f28cd2e3b57a0bdffc4ade6e610eb22a27f1027e
Kosei Moriyama <cou929@gmail.com>
2009-09-21 23:12:38 +0900

    Add morph directory
    Add new directory for new feature test "morphorogy"

---
+++ works/morph/morph.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <set>
+#include <utility>
+#include "opencv/cv.h"
+#include "opencv/cvaux.h"
+#include "opencv/cxcore.h"
+#include "opencv/highgui.h"
+
+using namespace std;
+
+int main(int argc, char ** argv)
+{
+  return 0;
+}
